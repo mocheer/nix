3fb4ccb10afb7a1c07ae07790413ea26ca8a8a6d
mocheer <mocheer@foxmail.com>
2021-04-26 18:45:23 +0800

    update

--- cmds/about.go
+++ cmds/about.go
@@ -2,6 +2,7 @@ package cmds
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/mocheer/pluto/sys"
 	"github.com/urfave/cli/v2"
@@ -12,7 +13,12 @@ var About = &cli.Command{
 	Name:  "about",
 	Usage: "相关信息",
 	Action: func(c *cli.Context) error {
-		fmt.Println(sys.GetCurrentPath())
+		exePath, _ := sys.GetCurrentPath()
+		execPath, _ := os.Getwd()
+		fmt.Printf(`
+%c[1;31;40m---关于---%c[0m
+执行程序的所在位置：%s
+当前程序执行位置：%s`, 0x1B, 0x1B, exePath, execPath)
 		return nil
 	},
 }
--- cmds/run.go
+++ cmds/run.go
@@ -6,12 +6,15 @@ import (
 	"regexp"
 	"strings"
 
+	"github.com/mocheer/pluto/fn"
 	"github.com/mocheer/pluto/fs"
 	"github.com/mocheer/pluto/reg"
+	"github.com/mocheer/pluto/sys"
 	"github.com/urfave/cli/v2"
 )
 
 type Config struct {
+	Name    string
 	Scripts map[string]string
 }
 
@@ -26,18 +29,27 @@ var Run = &cli.Command{
 			scriptName := c.Args().Get(0)
 			scriptContent := conf.Scripts[scriptName]
 			if scriptContent != "" {
-				args := regexp.MustCompile(reg.CommandParams).FindAllString(scriptContent, 10)
-				params := []string{}
-				// 命令行参数在窗口输入的时候需要带引号，但这里的参数不需要，反而要去掉
-				for _, val := range args[1:] {
-					params = append(params, strings.ReplaceAll(val, `"`, ``))
+				scriptContent = fn.FmtString(scriptContent, map[string]interface{}{
+					"appName":  conf.Name,
+					"execPath": sys.GetAbsolutePath(),
+				})
+				fmt.Println(scriptContent)
+
+				scriptArray := strings.Split(scriptContent, "&&")
+				for _, script := range scriptArray {
+					args := regexp.MustCompile(reg.CommandParams).FindAllString(script, 10)
+					params := []string{}
+					// 命令行参数在窗口输入的时候需要带引号，但这里的参数不需要，反而要去掉
+					for _, val := range args[1:] {
+						params = append(params, strings.ReplaceAll(val, `"`, ``))
+					}
+					command := exec.Command(args[0], params...)
+					err := command.Run()
+					if err != nil {
+						fmt.Println(err)
+					}
 				}
-				fmt.Println(params)
-				command := exec.Command(args[0], params...)
-				err := command.Run()
-				if err != nil {
-					fmt.Println(err)
-				}
+
 			}
 		}
 		return nil
